25b8033e24fcca6a6c725234527fc1f4de0c95f8
Joe Server <jserver@broadway.com>
2012-07-11 16:47:12 -0400

    a few adjustments for ec2remote, probably a dead end script with so much bash invovled

--- scripts/ec2remote.py
+++ scripts/ec2remote.py
@@ -10,9 +10,14 @@ import BaseHTTPServer
 HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8000 # Maybe set this to 9000.
 
+Q = os.environ['Q']
+
 
 def install_formula(name):
-    subprocess.call('ls -al', shell=True)
+    output = subprocess.check_output(os.path.join(Q, name), shell=True, executable='/bin/bash')
+    print output
+    output = subprocess.check_output(['sqs_message', 'install', 'finished'], shell=True, executable='/bin/bash')
+    print 'SQS_MESSAGE: ', output
 
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -20,16 +25,17 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_response(200)
         s.send_header("Content-type", "text/plain")
         s.end_headers()
+
     def do_GET(s):
         """Respond to a GET request."""
         s.send_response(200)
-        s.send_header("Content-type", "text/plan")
+        s.send_header("Content-type", "text/plain")
         s.end_headers()
 
         path_items = s.path[1:].split('/')
-        if len(path_items) == 2 and path_items[0] == 'formula':
+        if len(path_items) == 2 and path_items[0] == 'formulas':
             formula_name = path_items[1]
-            if os.access(formula_name, os.F_OK):
+            if os.access(os.path.join(Q, formula_name), os.F_OK):
                 s.wfile.write('installing %s' % formula_name)
                 p = multiprocessing.Process(target=install_formula, args=(formula_name,))
                 p.start()
